fcda207459ed1055f593b21e27eacb9e81198127
MarjonaZa <nasimova12345678@gmail.com>
2024-12-13 15:55:43 +0700

    models

--- drfsite/women/models.py
+++ drfsite/women/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
     time_updated = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
@@ -14,8 +15,7 @@ class Women(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True) #выяснить что такое db_inedx
-# это удобный способ указать Django автоматически оптимизировать запросы
-    # к определённым полям через индексацию.
+# это удобный способ указать Django автоматически оптимизировать запросы к определённым полям через индексацию.
     def __str__(self):
         return self.name
 
