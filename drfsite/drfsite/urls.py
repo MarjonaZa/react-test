633abdeb560a86bf2618c0abfc3b4ee23ffbccd2
MarjonaZa <nasimova12345678@gmail.com>
2024-12-13 15:57:56 +0700

    routers

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,26 +1,49 @@
-"""
-URL configuration for drfsite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from women.views import WomenApiiew
+from rest_framework import routers
+
+from women.views import *
+
+class MyCustomRouter(routers.SimpleRouter): # осн цель упростить написание программы связанный с написанием маршрутов для вьюсетов
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={'get': 'list'},
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={'get': 'retrieve'},
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        ),
+    ]
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women') #'^women/$' [name='men-list']>, суть basename в том что dvtcnj women идёт man как быховый
+# !!!! если нет кюерисет (queryset = Women.objects.all()) то бейснейм обязателен т.к это штука берёт имя для марщрута
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist', WomenApiiew.as_view()), #откуда здесь .as_view?
-    # as_view Он позволяет Django использовать классы в маршрутах, как если бы это были функции.
+    path('api/v1/', include(router.urls)),
 ]
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/womenlist', WomenListAPIView.as_view() ),
+#     path('api/v1/womenlist/<int:pk>/' , WomenAPIUpdate.as_view()),
+#     path('api/v1/womenAll/<int:pk>/' , WomenAPIAll.as_view()),
+# ]
+
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/womenlist' , WomenApiiew.as_view()), #откуда здесь .as_view?
+#     # as_view Он позволяет Django использовать классы в маршрутах, как если бы это были функции.
+#     path('api/v1/womenlist/<int:pk>/' , WomenApiiew.as_view())
+# ]
