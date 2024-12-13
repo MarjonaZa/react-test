3c15e5d486dcb36493e58e0ee73cfee7704de2e2
MarjonaZa <nasimova12345678@gmail.com>
2024-12-13 17:16:50 +0700

    ерооор

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -1,36 +1,26 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework import routers
-
 from women.views import *
 
-class MyCustomRouter(routers.SimpleRouter): # осн цель упростить написание программы связанный с написанием маршрутов для вьюсетов
-    routes = [
-        routers.Route(
-            url=r'^{prefix}$',
-            mapping={'get': 'list'},
-            name='{basename}-list',
-            detail=False,
-            initkwargs={'suffix': 'List'}),
-        routers.Route(
-            url=r'^{prefix}/{lookup}$',
-            mapping={'get': 'retrieve'},
-            name='{basename}-detail',
-            detail=True,
-            initkwargs={'suffix': 'Detail'}
-        ),
-    ]
-router = MyCustomRouter()
-router.register(r'women', WomenViewSet, basename='women') #'^women/$' [name='men-list']>, суть basename в том что dvtcnj women идёт man как быховый
-# !!!! если нет кюерисет (queryset = Women.objects.all()) то бейснейм обязателен т.к это штука берёт имя для марщрута
-print(router.urls)
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('api/v1/women/', WomenListAPIView.as_view()),
+    path('api/v1/women/<int: pk>/', WomenAPIUpdate.as_view()),
+    path('api/v1/womendelete/<int:pk>/', WomenAPIDestroy.as_view()),
 ]
+
+
+# router = MyCustomRouter()
+# router.register(r'women', WomenViewSet, basename='women') #'^women/$' [name='men-list']>, суть basename в том что dvtcnj women идёт man как быховый
+#!!!! если нет кюерисет (queryset = Women.objects.all()) то бейснейм обязателен т.к это штука берёт имя для марщрута
+#print(router.urls)
+
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
