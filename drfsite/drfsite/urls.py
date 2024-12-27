d26a1be8de389c39dbe0f79a9396ba78d5b0011c
MarjonaZa <nasimova12345678@gmail.com>
2024-12-27 15:31:24 +0700

    JWT

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from women.views import *
 
 urlpatterns = [
@@ -13,6 +15,9 @@ urlpatterns = [
 
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
 
 
